530df227b75c6a353493b87275aaafbbc721e576
pcheel <sochnyi_kun@mail.ru>
2023-06-24 11:12:58 +0300

    вариант загрузки изображений перед спавном (вариант неудачный)

--- Assets/Scripts/GeneralImageModel.cs
+++ Assets/Scripts/GeneralImageModel.cs
@@ -2,23 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Threading.Tasks;
 
 public class GeneralImageModel : IImageModel
 {
     private Sprite _image;
-    private ImagesLoader _imagesLoader;
+    // private ImagesLoader _imagesLoader;
 
     public Action<Sprite> ChangeImageAction{get;set;}
 
-    public GeneralImageModel(ImagesLoader imagesLoader)
+    // public GeneralImageModel(Sprite image)
+    // {
+    //     _image = image;
+    //     // ChangeImageAction?.Invoke(_image);
+    // }
+    public void SetImage(Sprite image)
     {
-        _imagesLoader = imagesLoader;
-    }
-    public async void LoadImage()
-    {
-        // Debug.Log("load");
-        _image = await _imagesLoader.TryLoadImage();
-        Debug.Log("load2");
+        _image = image;
         ChangeImageAction?.Invoke(_image);
     }
+    // public async Task<bool> LoadImage()
+    // {
+    //     Sprite image = await _imagesLoader.TryLoadImage();
+    //     if (image != null)
+    //     {
+    //         _image = image;
+    //         ChangeImageAction?.Invoke(_image);
+    //         return true;
+    //     }
+    //     else
+    //     {
+    //         return false;
+    //     }
+    // }
 }
--- Assets/Scripts/IFactory.cs
+++ Assets/Scripts/IFactory.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public interface IFactory
 {
     public IImageGallery CreateImageGallery(IFactory factory, ImagesLoader imagesLoader, GameObject imagePrefab, Transform imagesParent);
-    public IImageModel CreateImageModel(ImagesLoader imagesLoader);
+    public IImageModel CreateImageModel();
     public IImageView CreateImageView(GameObject imagePrefab, Transform parent);
     public ImagePresenter CreateImagePresenter(IImageModel imageModel, IImageView imageView);
 }
